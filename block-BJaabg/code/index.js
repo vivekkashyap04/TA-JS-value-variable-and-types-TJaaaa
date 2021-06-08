74e7d533a2515ae8f84f3efba27249cc31288c30
vivek <kumarvivek313@gmail.comgit>
2021-06-08 22:49:42 +0530

    A-03

--- block-BJaabg/code/index.js
+++ block-BJaabg/code/index.js
@@ -14,5 +14,20 @@ const ACCESSORY_PRICE = 9.99;
 var bank_balance = 303.91;
 var amount = 0;
 // your code goes here
+for(let i=1; amount<bank_balance; i++){
+    amount += PHONE_PRICE;
+    if(amount < SPENDING_THRESHOLD){
+        amount += ACCESSORY_PRICE;
+        console.log(amount);
+    }
+}
+let taxAmount = (amount * TAX_RATE);
+let purchaseAmount = amount +taxAmount ;
+console.log("$" +purchaseAmount);
+if(purchaseAmount < bank_balance){
+    alert("afford!")
+}else{
+    alert("can't afford")
+}
 
 // ⛑ Answer of the above will `$334.76`.
